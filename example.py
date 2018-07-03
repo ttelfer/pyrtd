0c7019d335f820c3321883a1a4c7be475afbbcbf
Trent Telfer <trent.telfer@gmail.com>
2018-07-03 16:12:21 -0600

    First stab at getting and RTD client/server imlementation working.

---
+++ example.py
@@ -0,0 +1,10 @@
+from pyrtd_client import RTDClient
+
+def main():
+    return
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        print("Ctrl+C pressed....")
